3709ab6ca562df68bbedb19e071a5988205d627d
Jack Morrison <jack@morrison.ec>
2025-02-10 18:52:52 +0000

    Fixed issue when the config.toml was being overritten each time.

--- boot.sh
+++ boot.sh
@@ -3,10 +3,19 @@
 SOURCE_DIR="/discordBotFiles/"
 DEST_DIR="/home/container"
 
+SOURCE_FILE="config.template.toml"
+DEST_FILE="config.toml"
+
 rsync -av --ignore-existing "$SOURCE_DIR" "$DEST_DIR"
 
 cd '/home/container'
-cp config.template.toml config.toml
+
+if [ ! -f "$DEST_FILE" ]; then
+    echo "$DEST_FILE does not exist. Renaming $SOURCE_FILE to $DEST_FILE."
+    mv "$SOURCE_FILE" "$DEST_FILE"
+else
+    echo "$DEST_FILE already exists. No action taken."
+fi
 
 npm install
 npm run start
