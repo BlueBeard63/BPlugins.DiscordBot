a17d60c92de39b09cf99e2c9763244b49738969c
BlueBeard63 <jack@morrison.ec>
2025-03-03 12:39:57 +0000

    Moved config options under categories, also added KofiStore category with port option.

--- src/environment.ts
+++ src/environment.ts
@@ -12,16 +12,18 @@ export class Config {
 
 const config = new Config();
 
-export const DISCORD_BOT_ID = config.data.DISCORD_BOT_ID as string;
-export const DISCORD_BOT_SECRET = config.data.DISCORD_BOT_SECRET as string;
+export const DISCORD_BOT_ID = config.data.DiscordBot.DISCORD_BOT_ID as string;
+export const DISCORD_BOT_SECRET = config.data.DiscordBot.DISCORD_BOT_SECRET as string;
 
-export const DATABASE_HOST = config.data.DATABASE_HOST as string;
-export const DATABASE_PORT = config.data.DATABASE_PORT as Number;
-export const DATABASE_USERNAME = config.data.DATABASE_USERNAME as string;
-export const DATABASE_PASSWORD = config.data.DATABASE_PASSWORD as string;
-export const DATABASE_NAME = config.data.DATABASE_NAME as string;
+export const DATABASE_HOST = config.data.Database.DATABASE_HOST as string;
+export const DATABASE_PORT = config.data.Database.DATABASE_PORT as Number;
+export const DATABASE_USERNAME = config.data.Database.DATABASE_USERNAME as string;
+export const DATABASE_PASSWORD = config.data.Database.DATABASE_PASSWORD as string;
+export const DATABASE_NAME = config.data.Database.DATABASE_NAME as string;
 
-export const COMMISSIONS_GUILD_ID = config.data.COMMISSIONS_GUILD_ID as string;
-export const COMMISSIONS_CHANNEL_ID = config.data.COMMISSIONS_CHANNEL_ID as string;
-export const COMMISSIONS_DEV_ROLE_ID = config.data.COMMISSIONS_DEV_ROLE_ID as string;
-export const DEFAULT_ROLE_IDS = (config.data.DEFAULT_ROLE_IDS as string)!.split(',');
+export const COMMISSIONS_GUILD_ID = config.data.DiscordServer.COMMISSIONS_GUILD_ID as string;
+export const COMMISSIONS_CHANNEL_ID = config.data.DiscordServer.COMMISSIONS_CHANNEL_ID as string;
+export const COMMISSIONS_DEV_ROLE_ID = config.data.DiscordServer.COMMISSIONS_DEV_ROLE_ID as string;
+export const DEFAULT_ROLE_IDS = (config.data.DiscordServer.DEFAULT_ROLE_IDS as string)!.split(',');
+
+export const KofiServerPort = config.data.KofiStore.Port as number;
